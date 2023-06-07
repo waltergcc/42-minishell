55057b74131d4d8427733bf49920715d54edce05
waltergcc <waltergccruz@gmail.com>
2023-06-07 09:30:44 +0100

    remove msh->commands check in run cmd fix school

--- minishell/src/commands.c
+++ minishell/src/commands.c
@@ -6,7 +6,7 @@
 /*   By: wcorrea- <wcorrea-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 00:53:17 by wcorrea-          #+#    #+#             */
-/*   Updated: 2023/06/06 10:07:46 by wcorrea-         ###   ########.fr       */
+/*   Updated: 2023/06/07 09:27:14 by wcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ void	check_redirections(t_shell *msh)
 void	run_command(t_shell *msh)
 {
 	check_redirections(msh);
-	if (msh->commands && msh->commands[0][0] != '>')
+	if (msh->commands[0][0] != '>')
 	{
 		get_tokens(msh);
 		if (msh->tokens[0])
